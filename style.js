946446d5b050ff0296130b14f7c2f2cfb757d05c
PotechinK <zenzen.ok.no.problem@gmail.com>
2026-06-01 20:50:15 +0900

    座右の銘が正面で停止する
    Added a pause feature to the heading rotation.

--- style.js
+++ style.js
@@ -1,13 +1,28 @@
 let heading = document.getElementById('heading');
 let degree = 0;
+let paused = false;
+
 function rotateHeading(){
-  degree = degree +3;
-  degree = degree % 360;
+  if (paused) return;
+
+  degree = degree + 3;
+
+  if (degree >= 360) {
+    degree = 0;
+    paused = true;
+
+    setTimeout(() => {
+      paused = false;
+    }, 1000);
+  }
+
   if(90 < degree && degree <= 270){
     heading.setAttribute('class', 'back');
   }else{
     heading.setAttribute('class', 'face');
   }
+
   heading.style.transform = 'rotateX(' + degree + 'deg)';
 }
+
 setInterval(rotateHeading, 50);
